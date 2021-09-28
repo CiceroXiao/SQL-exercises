077036c5315451d5c980e413d500e507201cb7f7
Cicero Xiao <ciceroxiao@gmail.com>
2021-09-28 20:52:52 +0800

    complete day024 and improve the contents of the template

--- SQL-daily-exercises/day-model-answer.sql
+++ SQL-daily-exercises/day-model-answer.sql
@@ -1,15 +1,18 @@
 /*
-所属项目：SQL-daily-exercises
-所属数据库：SQL_daily_exercises
-语言运行环境：MySQL 8.0
-原题链接：
-创建者：Cicero
-创建日期：
-修改日期：
-
-Cicero 的解题思路：
-*/
-
+ 所属项目：SQL-daily-exercises
+ 所属数据库：SQL_daily_exercises
+ 语言运行环境：MySQL 8.0
+ 原题链接：
+ 创建者：Cicero
+ 创建日期：
+ 修改日期：
+ 
+ Cicero 的解题思路：
+ 1. 审题：
+ 2. 案例：
+ 3. 公式：
+ 4. 实现：
+ */
 SHOW DATABASES;
 
 USE sql_daily_exercises;
